1c1c6b3cedf461d3feb26f0388fe09097b42dc04
Kevin Do <kevindokhoale@gmail.com>
2017-12-30 00:19:58 -0800

    Connect/Disconnect Client Test

--- Assets/Scripts/Network.cs
+++ Assets/Scripts/Network.cs
@@ -5,14 +5,23 @@ using SocketIO;
 public class Network : MonoBehaviour
 {
 	static SocketIOComponent socket;
+	public GameObject playerPrefab;
 
 	// Use this for initialization
 	void Start ()
 	{
 		socket = GetComponent<SocketIOComponent>();
 		socket.On("open", OnConnected);
+		socket.On("spawn", OnSpawn);
+
 	}
 
+	void OnSpawn(SocketIOEvent e)
+	{
+		Debug.Log("Spawned");
+		Instantiate(playerPrefab);
+	}
+	
 	void OnConnected(SocketIOEvent e)
 	{
 		Debug.Log("connected");
--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,6 @@
 defaultPort = 3000;
 var io = require("socket.io")(process.env.port|| defaultPort);
-
+var playerCount = 0;
 console.log("Server started");
 
 io.on("connection", function(socket){
@@ -8,8 +8,18 @@ io.on("connection", function(socket){
 
     //Broadcast to all connected clients:
     socket.broadcast.emit("spawn");
-
+    playerCount++;
+    for (i = 0; i < playerCount; i++){
+        //Only sent to newly connected clients
+        socket.emit("spawn");
+        console.log("Sending spawn action to new client.");
+    }
     socket.on("move", function(data){
         console.log("Client moved!");
+    });
+
+    socket.on("disconnect", function(){
+        console.log("Client disconnected.");
+        playerCount--;
     });
 })
